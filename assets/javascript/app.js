624e01aee3e3420570325890e01c76cd2c103bd9
peak <thonen.jacob@gmail.com>
2018-09-19 20:42:45 -0700

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,36 @@
 $(document).ready(function(){
    
+    var timeset = 10;
+    var counter = setInterval(timer, 1000)
+    var correctanswers = 0;
+    var incorrectanswes = 0;
+    var unanswer = 0;
+
+    hidestart();
+
+    function hidestart() {
+        $(".qone").hide();
+        $(".qtwo").hide();
+        $(".qthree").hide();
+        $(".qfour").hide();
+    }
+
+    function countdown() {
+        timeset = timeset-1; 
+        if (timeset <= 0) {
+            clearInterval(counter);
+            return;
+        }
+        
+    }
+
+    $(".start-button").on("click", function(){
+        $(".start").hide();
+        $(".qone").show();
+        countdown();
+        
+    })
+
+
+
 });
